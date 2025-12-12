49ed6c0232321f7fb51a3828bd9a255e90cae0fb
Johan Suarez <jossuarperdom@gmail.com>
2025-12-11 23:08:10 -0500

    fix: relational model and setup proyect

--- src/main/java/org/example/videitos/controllers/LoginController.java
+++ src/main/java/org/example/videitos/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package org.example.videitos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
